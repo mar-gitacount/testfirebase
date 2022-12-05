4e0e343102de5a96f936ac7bd868d1d7350c0772
市川マサル <ichikawa.contact@gmail.com>
2022-12-06 00:54:21 +0900

    ユーザー登録画面切り替え調整

--- testfirebase/MainView.swift
+++ testfirebase/MainView.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseFirestore
+import LocalAuthentication
 struct ListArray: Identifiable {
     let id: Int
     let name: String
@@ -13,7 +17,146 @@ struct ListArray: Identifiable {
     var test:View_Standard = View_Standard()
 }
 
+struct MaintestView: View {
+    /**ログインに成功したら、配列を空にして成功したコードの中で再度配列を入れ込む */
+    @State var subViewItems: [SubtestViewItem] = []
+    @State var selectiontag = 1
+    @State var flgNo = 1
+    
+    
+//    self.subViewItems.append(SubtestViewItem())
+    init() {
+           UITabBar.appearance().isHidden = false
+        
+       }
+    
+    var body: some View {
+        VStack {
+            /**ここで呼び出しビューの設定をする。 */
+            if(flgNo==2){
+                let Viewc = UserView()
+                TabView(selection:$selectiontag){
+                    UserView().tabItem{
+                        Image(systemName: "message.fill")
+                    }.tag(1)
+                }
+            }else{
+                TabView(selection:$selectiontag){
+                    UserCheck(flgNo:$flgNo).tabItem{
+                        Image(systemName: "face.smiling.fill")
+                    }.tag(2)
+                }
+            }
+                NavigationView{
+                    Button("ADD", action: {
+                        /**押すたびにviewを描画する。 */
+                        self.flgNo = 1
+//                        if(subViewItems.isEmpty){
+//                            self.subViewItems.append(SubtestViewItem())
+//                        }
+                    })
+                }
+//            ForEach(subViewItems) { item in
+//                SubView(item: item)
+//            }
+//            TabView(selection:$selectiontag){
+//                UserView()
+//                    .tabItem {
+//                        Image(systemName: "message.fill")
+//                        Text("メッセージ")
+//                    }.tag(1)
+//                UserView()
+//                    .tabItem {
+//                        Image(systemName: "message.fill")
+//                        Text("メッセージ")
+//                    }.tag(2)
+//            }.onChange(of:selectiontag){
+//                selection in
+//                if(selection == 2){
+//                    //self.subViewItems.append(SubtestViewItem())
+//                    //UserView().removeFromSuperview()
+//                }
+//                
+//            }
+//            ForEach(0..<subViewItems.count, id: \.self) { index in
+//                SubView(item: subViewItems[index])
+//            }
+        }
 
+    }
+}
+
+struct SubtestViewItem: Identifiable {
+    let id = UUID()
+
+}
+
+struct SubView: View {
+    let item: SubtestViewItem
+    
+    var body: some View {
+            UserView()
+        
+        
+    }
+}
+
+
+
+
+/**ここのクラスを監視してpublishedを制御する */
+//class Change_User_Flg:ObservableObject{
+//    @Published var UserViewChangeFlg :Bool
+//    @Published var test :Bool
+//    /**ユーザーフラグを初期化する */
+//    init(UserViewChangeFlg:Bool){
+//        self.UserViewChangeFlg = false
+//        self.test = true
+//    }
+//}
+final class Tab_Insert{
+    private init () {}
+    static var shard = Tab_Insert()
+//    var usertab: = UserView()
+//    init(usertab:NSObject){
+//        self.usertab = usertab
+//    }
+
+}
+final class Change_User_flg{
+    /**ここでユーザーデータを格納して、ビューの呼び出し先では、ユーザーnameの有無を判定する。*/
+    // イニシャライズ
+    private init() {}
+    /**UserCheckでもここを参照してログインが成功すれば、ここに格納する。 */
+    static var shared = Change_User_flg()
+    var tabViewChange = Tab_View_change()
+    var changePage = Userpage()
+    // ユーザID
+      var id: String = "ll"
+      // ユーザ名
+      var name: String = ""
+    init(id:String,name:String){
+        self.id = id
+        self.name = name
+    }
+    func addValue(){
+        self.tabViewChange.id = id
+    }
+    func changedUserpage(){
+        self.changePage.flg = true
+    }
+      // ユーザ情報セットメソッド
+//      public static func setChange_User_flg(id: String, name: String) {
+//          self.id = id
+//          self.name = name
+//      }
+    
+}
+
+struct Tab_View_change{
+    var id:String = "test"
+    
+}
 /**トピックス補完の構造体 */
 struct View_Standard{
     var main_text_array:[String] = ["ホーム","探す","通知"]
@@ -44,14 +187,111 @@ func List_text_back() -> [ListArray] {
 //    }
     return share.main_text_array
 }
+func TabControll() -> TabViewseCotroll{
+//    let share = Change_User_flg.shared.self.flg
+    //var name: String
+    let change_user_flg = Change_User_flg.init(id: "testid", name: "testname")
+    return TabViewseCotroll()
+}
 
+
+/**このクラス内の変数を制御して、MainViewで関数を呼び出すたびに以下の構造体のTabViewが切り替わるようにする。
+その為にはユーザーログインに成功するたびに、フラグを切り替える。 */
+/**呼び出される度にEmptyViewすれば、タブビューの被りはないかも、、 */
+struct TabViewseCotroll: View{
+    @State var selectionTag = 1
+    //@ObservedObject var viewModel = Change_User_Flg(UserViewChangeFlg:true)
+    //@State var test = viewModel.UserViewChangeFlg
+//    viewModel.UserViewChangeFlg = true
+//    if (userflgclass){
+//        print("")
+//    }
+    var body: some View{
+        return TabView(selection:$selectionTag){
+            
+        }
+    }
+}
+func usercheck()->Bool{
+    /**この関数でアップルログインか通常ログインかを選別する。
+     なぜなら、アップルログインか通常ログインかでも選別する方法が異なるため*/
+    var usercheck = Change_User_flg.shared.id
+    /**ここでユーザー判定 */
+    if usercheck != ""{
+        return true
+    }
+    return false
+    
+}
+
+struct AnyviewItew:Identifiable{
+    let id = UUID()
+}
+
+struct testAnyView: View {
+    @State private var selectiontabindex = 0
+    //var testanyview:AnyView
+    let views:[AnyviewItew] = []
+    @State var flgNo = 1
+    var body: some View{
+
+        VStack{
+//            Spacer()
+//            showView()
+//            Spacer()
+            TabView(selection:$selectiontabindex){
+                ForEach(0..<self.views.count){ index in
+                    showView().tabItem{
+                        Image(systemName: "magnifyingglass")
+                        //Text(self.views[index])
+                    }.tag(index)
+                }
+            } .accentColor(.green)
+        }
+    }
+    private func showView() -> AnyView{
+        switch self.selectiontabindex{
+        case 1:
+//            usercheck() == true ? return AnyView(Userpage()):return AnyView(UserCheck())
+            if(usercheck()){
+                return AnyView(Userpage())
+            }else {
+                return AnyView(UserCheck(flgNo:$flgNo))
+            }
+
+        default:
+            return AnyView(MemberView())
+        }
+    }
+}
+/**レンダリングして毎回関数を呼び出す動き */
+/**レンダリングするにはstructを引き継いで、参照先の構造体をクラスが更新する。 */
 struct MainView: View {
+    static var kazu = 1
     @State private var ViewStandard = View_Standard()
     @State private var Textarray = View_Standard.init(main_text_array:)
-    /**ここを関数にする。 */
+    @State private var userviewflg = false
+    var rink = "メインのプロパティ"
+    /**管理下する事でタブを動的に切り替えるようにする。
+     呼び出し先でフラグを判定し、タブの内容を切り替える。
+     */
+    //@State private var TabChangeBar = TabControll()
+    /**切り替えフラグを監視下に置き、切り替える度にTabChangeBarが動的に機能するようにする。切り替えフラグは共通インスタンス */
+//    @StateObject private var test  = Change_User_flg()
     @State private var Viewcontroll = MemberView()
     @State private var selectedTag = 1
+    @State var flgNo = 1
+    /**以下はidフラグ */
+    @State private var change_user_flg = Change_User_flg.shared.id
+    @State private var changetab = Change_User_flg.shared
+    //@State private var Usercheck  = UserCheck(flgNo:$flgNo)
+//    @State private var tab = Tab_Insert.shard.usertab
+    var test = "テスト"
+    //@ObservedObject var viewModel = Change_User_Flg(UserViewChangeFlg:true)
+    //@State private var view = viewModel.(UserViewChangeFlg:true)
     var body: some View {
+        //testAnyView()
+        //MaintestView()
         /**リスト一覧 */
         //let list = List_text_back()
         //Viewcontroll
@@ -64,25 +304,41 @@ struct MainView: View {
 //                }.tag(list.id)
 //            }
 //        }.accentColor(.green)
-        /**関数を呼ぶ */
-        TabView(selection:$selectedTag){
-            Viewcontroll
-                        .tabItem {
-                            Image(systemName: "message.fill")
-                            Text("メッセージ")
-                        }.tag(1)
-            Viewcontroll
-                        .tabItem {
-                            Image(systemName: "magnifyingglass")
-                            Text("さがす")
-                        }.tag(2)
-                    UserCheck()
-                        .tabItem {
-                            Image(systemName: "face.smiling.fill")
-                            Text("スタンプ")
-                        }
+        /**ここでTabview切り替え */
+        /**以下を関数化する。 */
+        /**TabChangeBarをセットする。 */
+        /**publicな関数を直接参照しにいくか。 */
+        if(userviewflg){
+            TabViewseCotroll()
+            /**参照型usercheckに対して直接userpageを代入する。*/
+        }
+            TabView(selection:$selectedTag){
+                Viewcontroll
+                    .tabItem {
+                        Image(systemName: "message.fill")
+                        Text("メッセージ")
+                    }.tag(1)
+                Viewcontroll
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("さがす")
+                    }.tag(2)
+                UserCheck(flgNo:$flgNo)
+                    .tabItem {
+                        Image(systemName: "face.smiling.fill")
+                        Text(test)
+                    }.tag(3)
+            }
+            .accentColor(.green).onChange(of:selectedTag){
+                selection in
+                if selection == 3{
+                    changetab.changedUserpage()
+                    usercheck() == true ?  print("ユーザーページ"):print("ユーザーチェックページ")
+                    changetab.addValue()
+                    // test = Change_User_flg.shared.id
+                    
                 }
-                .accentColor(.green) //ここで色の指定
+            }//ここで色の指定
     }
 }
 
@@ -95,6 +351,6 @@ struct Mainview_animaiton: View{
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MainView().environmentObject(Model())
     }
 }
--- testfirebase/MemberView.swift
+++ testfirebase/MemberView.swift
@@ -15,9 +15,11 @@ import LocalAuthentication
 struct MemberView: View {
     /**外部firebaseと接続クラスのインスタンス */
     @ObservedObject var viewModel = Model()
+
     //@State var test = self.viewModel.ref_Simple_get()
     @State var selectedTag = 1
     var body: some View {
+        /**ここでログイン非ログインを切り分けする。 */
         HomeTabView()
 //        TabView(selection: $selectedTag){
 //            HomeTabView()
@@ -57,13 +59,37 @@ struct HomeTabView: View{
     }
 }
 struct UserView: View{
+    @State var maintestview = MaintestView()
+
     var body: some View {
-        Text("")
+        return
+        VStack{
+            Text("ユーザーページ!!")
+            Button("empty",action: {
+                self.maintestview.subViewItems.removeAll()
+            })
+        }
+       
+    }
+}
+struct Userpage :View{
+    @State var flg:Bool = false
+    var body: some View {
+        if(self.flg){
+            UserView()
+        }else{
+            UserView()
+        }
     }
 }
 /**ユーザーチェックしてtrueならユーザーページに遷移する。ー */
+/**通常ログインとアップルログインで処理を分ける。 */
 struct UserCheck :View {
     let context = LAContext()
+    //@State private var testmain = MainView()
+    @State var maintestview = MaintestView()
+    @Binding var flgNo: Int
+    @ObservedObject var viewModel = Model()
     @State private var signInWithAppleObject = SignInWithAppleObject()
     @State private var email: String = "ichikawa.contact@gmail.com"
     @State private var email_error: String = ""
@@ -76,137 +102,159 @@ struct UserCheck :View {
     @State private var editting = false
     @State private var email_edittiig = false
     @State private var password_editting = false
+    /**以下でタブビューをコントロールできるか検証 */
+    @State private var change = Change_User_flg.shared
+    //@State private var id = change.id
+
+    @State private var test = "test"
     @AppStorage("isOnbord") var isOnbord: Bool = true
     @AppStorage("count_key") var counter = 0
     //ここでログインフラグ判定ステート=toggleで変更監視している。
+    /**ここをデフォでfalseを代入していると延々とビューがコントロールできない。 */
     @State private var user_flg : Bool = false
     //let usertest = Auth_user_get_data(name:"name")
     var body: some View {
-        if(user_flg){
-            //UserView画面へ遷移する。
-            UserView()
+        if(flgNo == 2){
+            VStack{
+                Text("テストユーザー")
+                Button("ログアウト",action:{
+                    self.flgNo = 1
+                })
+            }
         }
-        VStack {
-            
-            //入力文字大文字を設定しない。
-            Text(email_error).foregroundColor(Color.red)
-            TextField("E-mail", text: self.$email,onEditingChanged: {
-                begin in
-                /// 入力開始処理
-                if begin {
-                    self.email_edittiig = true    // 編集フラグをオン
-                    self.email = ""       // メッセージをクリア
-                /// 入力終了処理
-                } else {
-                    self.email_edittiig = false   // 編集フラグをオフ
-                }
-            }).autocapitalization(.none).textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
-                .padding()// 余白を追加
+        else{
+            VStack {
+                
+                Text(test)
+                //入力文字大文字を設定しない。
+                Text(email_error).foregroundColor(Color.red)
+                TextField("E-mail", text: self.$email,onEditingChanged: {
+                    begin in
+                    /// 入力開始処理
+                    if begin {
+                        self.email_edittiig = true    // 編集フラグをオン
+                        self.email = ""       // メッセージをクリア
+                        /// 入力終了処理
+                    } else {
+                        self.email_edittiig = false   // 編集フラグをオフ
+                    }
+                }).autocapitalization(.none).textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
+                    .padding()// 余白を追加
                 // 編集フラグがONの時に枠に影を付ける
-                .shadow(color: email_edittiig ? .blue : .clear, radius: 3)
-            Text(password_error).foregroundColor(Color.red)
-            TextField("Password", text: self.$password,onEditingChanged: { begin in
-                /// 入力開始処理
-                if begin {
-                    self.password_editting = true    // 編集フラグをオン
-                    self.password = ""       // メッセージをクリア
-                /// 入力終了処理
-                } else {
-                    self.password_editting = false   // 編集フラグをオフ
-                }
-            }).keyboardType(.default).autocapitalization(.none).textFieldStyle(RoundedBorderTextFieldStyle()).padding() .shadow(color: password_editting ? .blue : .clear, radius: 3)
-            //ログイン方法はアップルログインと、通常のログインを実装する。
-            //falseが帰ってきた場合、登録画面にリダイレクトする。
-            //入力値を制御す
-            Button(action: {
-                //userのAuth
-                self.email_error = ""
-                self.password_error = ""
-                if(self.email == ""){
-                //ここでfalseを返す
-                    self.email_error = "メールアドレスを入力してください。"
-                }else if(self.password == ""){
-                    self.password_error = "パスワードを入力してください。 "
-                }else if (user_flg){
-                    //Userview()
-                }
-                else{
-                    //ここでサインイン設定。trueなら画面遷移して、会員のページにいく。
-                    Auth.auth().signIn(withEmail:email,password: password) { (resurt,error) in
-                       //認証で何かしらのエラーで実行される条件
-                       if error != nil{
-                          //エラーコードの変数を設定する。
-                          //ここで登録画面打診メッセージを表示する。
-                         self.email_error = "登録がありません"
-                          //let errorCode = AuthErrorCode(rawValue: error.code)
-                          //self.alertMessage = error?.localizedDescription ?? ""
-                          self.alertMessage = "Error"
-                          self.isShowAlert = true
-                        
-                        }else{
-                            //認証が成功した時の処理を書く
-                            //ログイン成功したら、それにひもづくデータを返す
-                            //会員ページに画面遷移する。
-                            //self.email_error = user.email
-                            //画面遷移する。
-                            let user = Auth.auth().currentUser
-                            let ref = Database.database().reference()
-//                            let alovelaceDocumentRef = ref.child("users").child("1AzjIctS3DOvIZHOHkmmfcCIXGi1").child(name)
-                            //1AzjIctS3DOvIZHOHkmmfcCIXGi1
-                            //成功したら画面を遷移してユーザーページを表示する。
-                            if let user = user {
-                                let uids = user.uid
-                                let _ = print(uids)
-                                let email = user.email
-                                let db = Firestore.firestore()
-                                //firestoreでuserコレクション内のドキュメントを取得する。
-                                let userget = db.collection("users").document("1AzjIctS3DOvIZHOHkmmfcCIXGi1")
+                    .shadow(color: email_edittiig ? .blue : .clear, radius: 3)
+                Text(password_error).foregroundColor(Color.red)
+                TextField("Password", text: self.$password,onEditingChanged: { begin in
+                    /// 入力開始処理
+                    if begin {
+                        self.password_editting = true    // 編集フラグをオン
+                        self.password = ""       // メッセージをクリア
+                        /// 入力終了処理
+                    } else {
+                        self.password_editting = false   // 編集フラグをオフ
+                    }
+                }).keyboardType(.default).autocapitalization(.none).textFieldStyle(RoundedBorderTextFieldStyle()).padding() .shadow(color: password_editting ? .blue : .clear, radius: 3)
+                //ログイン方法はアップルログインと、通常のログインを実装する。
+                //falseが帰ってきた場合、登録画面にリダイレクトする。
+                //入力値を制御す
+                Button(action: {
+                    //userのAuth
+                    self.email_error = ""
+                    self.password_error = ""
+                    if(self.email == ""){
+                        //ここでfalseを返す
+                        self.email_error = "メールアドレスを入力してください。"
+                    }else if(self.password == ""){
+                        self.password_error = "パスワードを入力してください。 "
+                    }else if (user_flg){
+                        //Userview()
+                    }
+                    else{
+                        //ここでサインイン設定。trueなら画面遷移して、会員のページにいく。
+                        Auth.auth().signIn(withEmail:email,password: password) { (resurt,error) in
+                            //認証で何かしらのエラーで実行される条件
+                            if error != nil{
+                                //エラーコードの変数を設定する。
+                                //ここで登録画面打診メッセージを表示する。
+                                self.email_error = "登録がありません"
+                                //let errorCode = AuthErrorCode(rawValue: error.code)
+                                //self.alertMessage = error?.localizedDescription ?? ""
+                                self.alertMessage = "Error"
+                                self.isShowAlert = true
+                                
+                            }else{
+                                //認証が成功した時の処理を書く
+                                //ログイン成功したら、それにひもづくデータを返す
+                                //会員ページに画面遷移する。
+                                //self.email_error = user.email
+                                //画面遷移する。
+                                let user = Auth.auth().currentUser
+                                let ref = Database.database().reference()
+                                //                            let alovelaceDocumentRef = ref.child("users").child("1AzjIctS3DOvIZHOHkmmfcCIXGi1").child(name)
+                                //1AzjIctS3DOvIZHOHkmmfcCIXGi1
+                                //成功したら画面を遷移してユーザーページを表示する。
+                                if let user = user {
+                                    let uids = user.uid
+                                    let _ = print(uids)
+                                    let email = user.email
+                                    let db = Firestore.firestore()
+                                    //firestoreでuserコレクション内のドキュメントを取得する。
+                                    let userget = db.collection("users").document(uids)
                                     userget.getDocument { (document,error) in
                                         if let document = document, document.exists{
                                             let dataDesciption = document.data().map(String.init(describing:)) ?? "nil"
                                             self.email_error = "あるよ"
-                                            user_flg.toggle()
+                                            change.addValue()
+                                            change.id = "id"
+                                            self.flgNo=2
+                                            self.test = "test2"
+                                            //change.id = self.testmain.rink
+                                            //Change_User_flg.addValue()
+                                            //self.email_error = change_user_flg.id
+                                            /**userクラスをtrueにする。*/
+                                            //user_flg.toggle()
                                             NavigationLink(destination: UserView(),
-                                                                           isActive: $user_flg) {
-                                                                            EmptyView()
-                                                            }
-                                
+                                                           isActive: $user_flg) {
+                                                EmptyView()
+                                            }
+                                            
                                             //user_flg.toggle()
                                         } else {
                                             self.email_error = "ないよ"
                                         }
                                     }
-                               // self.email_error = self.email
+                                    // self.email_error = self.email
+                                }
+                                //ここで画面を遷移する。
+                                self.alertMessage = "Success"
+                                self.isShowAlert = true
                             }
-                            //ここで画面を遷移する。
-                            self.alertMessage = "Success"
-                            self.isShowAlert = true
                         }
                     }
-                }
-            }, label: {
-                /**明日はここから */
-                let userID = Auth.auth().currentUser!.uid
-                
-                Text(userID).font(.body).frame(width:350,height: 50).background(Color.black)
-                //isPresented=他の画面から呼ばれた場合の処理。
-            })//.alert(isPresented: $isShowAlert, content:{Alert(title: Text($alertMessage))}).padding().cornerRadius(60)
-              .accentColor(Color.white)
-                   Button(action: {
-                       signInWithAppleObject.signInWithApple()
-                       /**Appストレージに入れ込む */
-                       let userID = Auth.auth().currentUser!.uid
-                       self.name = userID
-                       //print(signInWithAppleObject.signInWithApple())
-                       //print("ここに表示される。")
-                      // signInWithAppleObject.authorizationController()
-                   }, label: {
-                       SignInWithAppleButton()
-                           .frame(height: 50)
-                           .cornerRadius(16)
-                   })
-                   .padding()
-               }
+                }, label: {
+                    /**明日はここから */
+                    //let userID = Auth.auth().currentUser!.uid
+                    
+                    Text("会員登録はこちらから").font(.body).frame(width:350,height: 50).background(Color.black)
+                    //isPresented=他の画面から呼ばれた場合の処理。
+                })//.alert(isPresented: $isShowAlert, content:{Alert(title: Text($alertMessage))}).padding().cornerRadius(60)
+                .accentColor(Color.white)
+                Button(action: {
+                    signInWithAppleObject.signInWithApple()
+                    /**Appストレージに入れ込む */
+                    let userID = Auth.auth().currentUser!.uid
+                    print(userID)
+                    //self.name = userID
+                    //print(signInWithAppleObject.signInWithApple())
+                    //print("ここに表示される。")
+                    // signInWithAppleObject.authorizationController()
+                }, label: {
+                    SignInWithAppleButton()
+                        .frame(height: 50)
+                        .cornerRadius(16)
+                })
+                .padding()
+            }
+        }
     }
 }
 /**ニュースのメインビュー */
